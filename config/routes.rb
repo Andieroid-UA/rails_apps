72432c693ea54c9799eaad9b0f88cbda7bcfe51b
Andres Astra <andieroid@arizona.edu>
2024-02-01 14:07:57 -0500

    .

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,16 @@ Rails.application.routes.draw do
   # localhost:3000/users/1
   get '/users/:id', to: 'users#show'
   
-  
+  # localhost:3000/users/1/posts
+  post '/users', to: 'users#create'
+
+  # localhost:3000/users/1
+  put '/users/:id', to: 'users#update'
+
+  # localhost:3000/users/1
+  delete '/users/:id', to: 'users#destroy'
+
+end
 #--------------------------------------------------------------------------
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -17,4 +26,3 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   #root "application#index"
-end
